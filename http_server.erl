013181495860582294c6a366acd3ba04f44ad048
casper <casper.zx@gmail.com>
2012-10-26 19:28:10 +0400

    Some rename in the HTTP server.
    http_server:
     - the callback name (and function) is changed to the request_handler
       one

--- http_server.erl
+++ http_server.erl
@@ -9,7 +9,7 @@
 -behaviour(gen_server).
 
 -export([start_link/0, start_link/1]).
--export([callback/1]).
+-export([request_handler/1]).
 -export([listen/1, close/0]).
 
 -export([init/1, terminate/2, code_change/3]).
@@ -39,48 +39,50 @@ end).
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 start_link(Args) -> gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
-callback(Module) -> gen_server:call(?MODULE, {callback, Module}).
+request_handler(Module) -> gen_server:call(?MODULE, {request_handler, Module}).
 
 listen(Port) -> gen_server:call(?MODULE, {listen, Port}).
 close() -> gen_server:call(?MODULE, close).
 
 init([]) -> process_flag(trap_exit, true), {ok, []};
-init([{config, FileName}, {callback, Module}]) ->
+init([{config, FileName}, {request_handler, Module}]) ->
 	process_flag(trap_exit, true),
 	{ok, Config} = file:consult(FileName),
 	{listen, {port, Port}} = lists:keyfind(listen, 1, Config),
 	{ok, LSocket} = gen_tcp:listen(Port, ?ListenOptions),
 	spawn_accept(Module, LSocket),
-	{ok, [{module, Module}, {lsocket, LSocket}]}.
+	{ok, [{request_handler, Module}, {lsocket, LSocket}]}.
 
-handle_call({callback, Module}, _From, [_OldModule, LSocket]) ->
-	{reply, ok, [{module, Module}, LSocket]};
-handle_call({callback, Module}, _From, _State) ->
-	{reply, ok, [{module, Module}]};
+handle_call({request_handler, Module}, _From, [_RequestHandler, LSocket]) ->
+	{reply, ok, [{request_handler, Module}, LSocket]};
+handle_call({request_handler, Module}, _From, _State) ->
+	{reply, ok, [{request_handler, Module}]};
 
-handle_call({listen, _}, _From, []) -> {reply, {error, no_callback}, []};
-handle_call({listen, Port}, _From, State = [{module, Module}]) ->
+handle_call({listen, _}, _From, []) ->
+	{reply, {error, no_request_handler}, []};
+handle_call({listen, Port}, _From, State = [{request_handler, Module}]) ->
 	case gen_tcp:listen(Port, ?ListenOptions) of
 		{ok, LSocket} ->
 			spawn_accept(Module, LSocket),
-			{reply, ok, [{module, Module}, {lsocket, LSocket}]};
+			{reply, ok, [{request_handler, Module}, {lsocket, LSocket}]};
 		Error -> {reply, Error, State}
 	end;
-handle_call({listen, _}, _From, State = [{module, _}, {lsocket, _}]) ->
-	{reply, {error, already_listening}, State};
+handle_call({listen, _}, _From,
+	State = [{request_handler, _}, {lsocket, _}]) ->
+		{reply, {error, already_listening}, State};
 
-handle_call(close, _From, [{module, Module}, {lsocket, LSocket}]) ->
-	{reply, gen_tcp:close(LSocket), [{module, Module}]};
+handle_call(close, _From, [{request_handler, Module}, {lsocket, LSocket}]) ->
+	{reply, gen_tcp:close(LSocket), [{request_handler, Module}]};
 handle_call(close, _From, State) -> {reply, {error, not_listening}, State}.
 
-handle_cast(accept, State = [{module, Module}, {lsocket, LSocket}]) ->
+handle_cast(accept, State = [{request_handler, Module}, {lsocket, LSocket}]) ->
 	spawn_accept(Module, LSocket), {noreply, State}.
 
 handle_info(Info, State) ->
 	io:format("Info: ~p~n", [Info]),
 	{noreply, State}.
 
-terminate(Reason, [{module, _}, {lsocket, LSocket}]) ->
+terminate(Reason, [{request_handler, _}, {lsocket, LSocket}]) ->
 	io:format("Terminate: ~p~n", [Reason]),
 	gen_tcp:close(LSocket);
 terminate(Reason, _State) ->
@@ -89,21 +91,21 @@ terminate(Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
-spawn_accept(Module, LSocket) ->
-	proc_lib:spawn_link(fun() -> accept(Module, LSocket) end).
+spawn_accept(RequestHandler, LSocket) ->
+	proc_lib:spawn_link(fun() -> accept(RequestHandler, LSocket) end).
 
-accept(Module, LSocket) ->
+accept(RequestHandler, LSocket) ->
 	{ok, Socket} = gen_tcp:accept(LSocket),
 	gen_server:cast(?MODULE, accept),
-	wait_data(Module, Socket).
+	wait_data(RequestHandler, Socket).
 
-wait_data(Module, Socket) -> receive
+wait_data(RequestHandler, Socket) -> receive
 	{tcp, Socket, Data} ->
 %		io:format("Data: ~p~n", [Data]),
 %		io:format("Read: ~p~n", [http_reader:read(
 %			Data, {fun wait_more_data/1, Socket})]),
-		handle_result(Module, Socket, request, http_reader:read(
-			Data, {fun wait_more_data/1, Socket})),
+		handle_result(RequestHandler, Socket, request,
+			http_reader:read(Data, {fun wait_more_data/1, Socket})),
 		ok = gen_tcp:close(Socket);
 	{tcp_closed, Socket} -> io:format("TCP closed~n", []);
 	{tcp_error, Socket, Reason} -> io:format("TCP error: ~p~n", [Reason])
@@ -115,9 +117,10 @@ wait_more_data(Socket) -> receive
 	{tcp_error, Socket, _Reason} -> {error, no_more_data}
 end.
 
-handle_result(Module, Socket, request, {ok, Request}) ->
-	handle_result(Module, Socket, response, Module:handle_request(Request));
-handle_result(_Module, Socket, response, {ok, Response}) ->
+handle_result(RequestHandler, Socket, request, {ok, Request}) ->
+	handle_result(RequestHandler, Socket, response,
+		RequestHandler:handle_request(Request));
+handle_result(_RequestHandler, Socket, response, {ok, Response}) ->
 	gen_tcp:send(Socket, ?HttpOK(Response));
-handle_result(_Module, Socket, _Result, {error, Reason}) ->
+handle_result(_RequestHandler, Socket, _Result, {error, Reason}) ->
 	gen_tcp:send(Socket, ?HttpError(Reason)).
