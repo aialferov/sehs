c8c2dca427100ab03166b895b1715d4511f710de
casper <casper.zx@gmail.com>
2013-02-21 23:21:12 +0400

    The reader returns more complete data of request.
    The request is now represented with method, path, version, headers,
    query and body to the handler.
    The reader is now trying to read the body not depending on the method.
    Removed restriction by method, now any method is allowed.

--- src/http_reader.erl
+++ src/http_reader.erl
@@ -13,46 +13,43 @@
 read(Request, ContFun) when is_list(Request) ->
 	read(read_request_line(Request, [], []), ContFun);
 
-read({request_line, {_, RequestUri, _}}, _ContFun) ->
-	read(read_request_uri(RequestUri, []));
+read({request_line, {Method, RequestUri, Version}}, _ContFun) ->
+	result({Method, read_request_uri(RequestUri, []), Version}, [], []);
 read({request_line, RequestLine, Rest}, ContFun) ->
 	read({{request_line, RequestLine}, read_header(Rest, [], [])}, ContFun);
 read({request_line_incomplete, Rest, Item, RequestLine}, ContFun) ->
 	read_cont(fun(MoreData) -> read(read_request_line(
 		Rest ++ MoreData, Item, RequestLine), ContFun) end, ContFun);
 
-read({{request_line, {"GET", RequestUri, _}}, {header, _, _}}, _ContFun) ->
-	read(read_request_uri(RequestUri, []));
-read({{request_line, {"POST", RequestUri, _}},
-	{header, Header, Rest}}, ContFun)
-->
-	{ok, {RequestUri, read_post(Header, Rest, ContFun)}};
-read({{request_line, _}, {header, _, _}}, _ContFun) ->
-	{error, method_not_allowed};
+read({{request_line, {Method, RequestUri, Version}},
+	{header, Header, Body}}, ContFun
+) ->
+	result({Method, read_request_uri(RequestUri, []), Version},
+		Header, read_body(Header, Body, ContFun));
 read({RequestLine = {request_line, _},
 	{header_incomplete, Rest, Item, Header}}, ContFun
 ) ->
 	read_cont(fun(MoreData) -> read({RequestLine, read_header(
 		Rest ++ MoreData, Item, Header)}, ContFun) end, ContFun).
 
-read({request_uri, Path}) -> {ok, {Path, []}};
-read({request_uri, Path, Query}) -> {ok, {Path, Query}}.
+result({Method, {request_uri, Path, Query}, Version}, Header, Body) ->
+	{ok, {{Method, Path, Version}, Header, Query, Body}}.
 
-read_post(Header, Body, ContFun) when is_list(Header) ->
-	read_post(lists:keyfind(?ContentLengthField, 1, Header), Body, ContFun);
-read_post({_, ContentLength}, Body, ContFun) ->
-	read_post(list_to_integer(ContentLength), Body,
+read_body(Header, Body, ContFun) when is_list(Header) ->
+	read_body(lists:keyfind(?ContentLengthField, 1, Header), Body, ContFun);
+read_body({_, ContentLength}, Body, ContFun) ->
+	read_body(list_to_integer(ContentLength), Body,
 		byte_size(list_to_binary(Body)), ContFun);
-read_post(false, Body, _ContFun) -> utils_http:read_query(Body).
+read_body(false, Body, _ContFun) -> Body.
 
-read_post(ContentLength, Body, BodySize, ContFun)
+read_body(ContentLength, Body, BodySize, ContFun)
 	when ContentLength > BodySize
 ->
-	read_cont(fun(MoreData) -> NewBody = Body ++ MoreData, read_post(
+	read_cont(fun(MoreData) -> NewBody = Body ++ MoreData, read_body(
 		ContentLength, NewBody, byte_size(list_to_binary(NewBody)), ContFun)
 	end, ContFun);
-read_post(_ContentLength, Body, _BodySize, ContFun) ->
-	read_post(false, Body, ContFun).
+read_body(_ContentLength, Body, _BodySize, ContFun) ->
+	read_body(false, Body, ContFun).
 
 read_cont(ReadFun, {ContFun, Arg}) -> case ContFun(Arg) of
 	{ok, MoreData} -> ReadFun(MoreData);
@@ -88,4 +85,4 @@ complete_header_field(FieldValue, [FieldName|Header]) ->
 read_request_uri("?" ++ T, Path) ->
 	{request_uri, lists:reverse(Path), utils_http:read_query(T)};
 read_request_uri([H|T], Path) -> read_request_uri(T, [H|Path]);
-read_request_uri([], Path) -> {request_uri, lists:reverse(Path)}.
+read_request_uri([], Path) -> {request_uri, lists:reverse(Path), []}.
