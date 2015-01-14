55d0fdaf50f980f60a24827ea1b69518b2df24fe
Anton I Alferov <anton@alferov.me>
2015-01-14 14:44:19 +0000

    Add complete list of basic HTTP codes.

--- src/sehs_handler.erl
+++ src/sehs_handler.erl
@@ -19,12 +19,48 @@
 	"/" ++ utils_app:get_key(vsn) ++ ?CRLF
 ).
 -define(Status(Code), case Code of
-	ok -> "200 OK";
-	bad_request -> "400 Bad Request";
-	not_found -> "404 Not Found";
-	method_not_allowed -> "405 Method Not Allowed";
-	internal_server_error -> "500 Internal Server Error";
-	service_unavailable -> "503 Service Unavailable"
+	ok                              -> "200 OK";
+	created                         -> "201 Created";
+	accepted                        -> "202 Accepted";
+	non_authoritative_information   -> "203 Non-Authoritative Information";
+	no_content                      -> "204 No Content";
+	reset_content                   -> "205 Reset Content";
+	partical_content                -> "206 Partial Content";
+
+	multiple_choices                -> "300 Multiple Choices";
+	moved_permanently               -> "301 Moved Permanently";
+	found                           -> "302 Found";
+	see_other                       -> "303 See Other";
+	not_modified                    -> "304 Not Modified";
+	use_proxy                       -> "305 Use Proxy";
+	% (unused)                      -> "306 (Unused)";
+	temporary_redirect              -> "307 Temporary Redirect";
+
+	bad_request                     -> "400 Bad Request";
+	unauthorized                    -> "401 Unauthorized";
+	payment_required                -> "402 Payment Required";
+	forbidden                       -> "403 Forbidden";
+	not_found                       -> "404 Not Found";
+	method_not_allowed              -> "405 Method Not Allowed";
+	not_acceptable                  -> "406 Not Acceptable";
+	proxy_authentication_required   -> "407 Proxy Authentication Required";
+	request_timeout                 -> "408 Request Timeout";
+	conflict                        -> "409 Conflict";
+	gone                            -> "410 Gone";
+	length_required                 -> "411 Length Required";
+	precondition_failed             -> "412 Precondition Failed";
+	request_entity_too_large        -> "413 Request Entity Too Large";
+	request_uri_too_long            -> "414 Request-URI Too Long";
+	unsupported_media_type          -> "415 Unsupported Media Type";
+	requested_range_not_satisfiable -> "416 Requested Range Not Satisfiable";
+	expectation_failed              -> "417 Expectation Failed";
+
+	internal_server_error           -> "500 Internal Server Error";
+	not_implemented                 -> "501 Not Implemented";
+	bad_gateway                     -> "502 Bad Gateway";
+	service_unavailable             -> "503 Service Unavailable";
+	gateway_timeout                 -> "504 Gateway Timeout";
+	http_version_not_supported      -> "505 HTTP Version Not Supported"
 end).
 
 -define(CrashResponse, {internal_server_error, {[], []}}).
